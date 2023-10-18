3f5502900f79728b41359874bdbbe19ebd4ae17b
Jing Qian <jqian30@stuy.edu>
2023-10-18 19:34:23 -0400

    working towards mucho faster code

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 let fog;
 let lightSources = [];
 
+const GRADIENT_STEP = 6;
+
 function setup() {
     createCanvas(800, 800);
     fog = createGraphics(width, height);
     fog.noFill();
-    fog.strokeWeight(1);
+    //fog.strokeWeight(GRADIENT_STEP);
+    fog.noStroke();
     lightSources.push({x: 400, y: 400, str: 300});
 }
 
@@ -16,15 +19,13 @@ function draw() {
     blendMode(BLEND);
     background(255);
 
-    if (frameCount % 3 == 0) {
-        //this is an expensive operation, do this every 3rd frame
-        fog.background(0); //set to black
-        for (const light of lightSources) {
-            //graded white circle at each light source
-            for (let i = 0; i < light.str; i++) {
-                fog.stroke(255, 255 - i);
-                fog.circle(light.x, light.y, i);
-            }
+
+    fog.background(0); //set to black
+    for (const light of lightSources) {
+        //graded white circle at each light source
+        for (let i = 0; i < light.str; i += GRADIENT_STEP) {
+            fog.fill(255, 255 - i);
+            fog.circle(light.x, light.y, i);
         }
     }
 
