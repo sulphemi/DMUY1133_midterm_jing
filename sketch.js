70cdf68bc5d5ee7c64241dce4f6d60d44e49c302
Jing Qian <jqian30@stuy.edu>
2023-11-08 21:48:43 -0500

    stripped down functions to usables

--- sketch.js
+++ sketch.js
@@ -1,21 +1,17 @@
 let fog;
-let lightSources = [];
 
 const GRADIENT_STEP = 7;
 
 function setup() {
     createCanvas(800, 800);
     fog = createGraphics(width, height);
-    lightSources.push({x: 400, y: 400, str: 200});
+    prepFogLayer(300);
 
     noCursor();
     img = loadImage("./witch.jpg");
 }
 
 function draw() {
-    if (lightSources.length > 1) lightSources.pop();
-    lightSources.push({x: mouseX, y: mouseY, str: 150});
-
     background(255);
     image(img, -100, -100);
 
@@ -24,36 +20,30 @@ function draw() {
     noStroke();
     circle(mouseX, mouseY, 10);
 
-    //resource-intensive so we only update every few frames
-    if (frameCount % 3 === 0) prepFogLayer();
-    drawFogLayer();
+    drawFogLayer(mouseX, mouseY);
 
     console.log(frameRate());
 }
 
-function prepFogLayer() {
+function prepFogLayer(r) {
     fog.background(0); //set to black
 
-    for (const light of lightSources) {
-        //graded white circle at each light source
+    //circular viewing window
+    fog.noStroke();
+    fog.fill(255);
+    fog.circle(fog.width / 2, fog.height / 2, r);
 
-        //circlular viewing field
-        fog.noStroke();
-        fog.fill(255);
-        fog.circle(light.x, light.y, light.str);
-
-        //vignette
-        fog.noFill();
-        fog.strokeWeight(GRADIENT_STEP);
-        for (let i = 0; i < 255; i += GRADIENT_STEP) {
-            fog.stroke(255, 255 - i);
-            fog.circle(light.x, light.y, light.str + i);
-        }
+    //vignette
+    fog.noFill();
+    fog.strokeWeight(GRADIENT_STEP);
+    for (let i = 0; i < 255; i += GRADIENT_STEP) {
+        fog.stroke(255, 255 - i);
+        fog.circle(fog.width / 2, fog.height / 2, r + i);
     }
 }
 
-function drawFogLayer() {
+function drawFogLayer(x, y) {
     blendMode(MULTIPLY);
-    image(fog, 0, 0);
+    image(fog, x - fog.width / 2, y - fog.height / 2);
     blendMode(BLEND); //reset blend mode
 }
